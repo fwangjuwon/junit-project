576e44c6158cc431c80501191f9250bc473cc043
fwangjuwon <fwangjuwon@naver.com>
2022-08-17 23:24:36 +0900

    BookService - 책등록하기

--- src/main/java/site/gaeddo/junitproject/service/BookService.java
+++ src/main/java/site/gaeddo/junitproject/service/BookService.java
@@ -1,8 +1,33 @@
 package site.gaeddo.junitproject.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import lombok.RequiredArgsConstructor;
+import site.gaeddo.junitproject.domain.Book;
+import site.gaeddo.junitproject.domain.BookRepository;
+import site.gaeddo.junitproject.web.dto.BookRespDto;
+import site.gaeddo.junitproject.web.dto.BookSaveReqDto;
+
+@RequiredArgsConstructor
 @Service
 public class BookService {
 
+    private final BookRepository bookRepository;
+
+    // 1. 책 등록
+
+    @Transactional
+    public BookRespDto 책등록하기(BookSaveReqDto dto) {
+        Book bookPS = bookRepository.save(dto.toEntity());
+        return new BookRespDto().toDto(bookPS);
+    }
+
+    // 2. 책 목록보기
+
+    // 3. 책 한건보기
+
+    // 4. 책 삭제
+
+    // 5. 책 수정
 }
